ed4ff96e4360bdac27db52108646b8bf3c8ff035
John Gorman <johngorman2@gmail.com>
2020-03-27 12:43:06 -0300

    Skip interactive grub upgrade.

--- recipes/apt_upgrade.rb
+++ recipes/apt_upgrade.rb
@@ -3,6 +3,8 @@
 # apt-get upgrade can hang while prompting the user for the console encoding.
 # https://superuser.com/questions/1332341/console-setup-causes-apt-get-update-to-hang
 #
+# No hints worked to make grub* upgrades non-interactive so we skip them here.
+#
 
 return if skip_recipe
 
@@ -15,7 +17,10 @@ bash 'apt_upgrade' do
     export DEBCONF_FRONTEND=noninteractive
 
     apt-get update -qq
+
+    apt-mark hold grub-common grub-pc grub-pc-bin grub2-common
     apt-get upgrade -y -qq
+    apt-mark unhold grub-common grub-pc grub-pc-bin grub2-common
 
     #{bash_ended}
   EOT
