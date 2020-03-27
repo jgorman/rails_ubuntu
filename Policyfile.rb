baf9833a9e9bd7ba4c744a3d1d1e117317392d56
John Gorman <johngorman2@gmail.com>
2020-03-27 15:16:39 -0300

    Kitchen uses kitchen_test named run list for setup.

--- Policyfile.rb
+++ Policyfile.rb
@@ -10,7 +10,9 @@ name 'rails_ubuntu'
 default_source :supermarket
 
 # run_list: chef-client will run these recipes in the order specified.
-run_list 'rails_ubuntu::setup_test'
+run_list 'rails_ubuntu::default'
+
+named_run_list 'kitchen_test', 'rails_ubuntu::setup_test'
 
 # Specify a custom source for a single cookbook:
 cookbook 'rails_ubuntu', path: '.'
